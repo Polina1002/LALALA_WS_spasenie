da38e183614cb7d734f9eb45d4959a10ad0de37c
Polina1002 <appolinaria9494@mail.ru>
2023-04-27 10:19:07 +0300

    lalala_ws_spasenie

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntupc/lalala_ws/src
+# Build directory: /home/ubuntupc/lalala_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("study_pkg")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntupc/lalala_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntupc/lalala_ws/devel;/home/ubuntupc/ydlidar_ws/devel;/home/ubuntupc/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntupc/lalala_ws/devel;/home/ubuntupc/ydlidar_ws/devel;/home/ubuntupc/catkin_ws/devel;/opt/ros/noetic' != '' else []
